236ae320e43d3b79a6f89d0753a3d9c87447aa2a
Edward Terry <edward.william.terry@gmail.com>
2019-02-24 10:05:56 -0800

    Clean up cpp

--- src/grasped_reconstruction.cpp
+++ src/grasped_reconstruction.cpp
@@ -4,13 +4,7 @@
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include <math.h>
-// #include <pcl_ros/point_cloud.h>
-// #include <pcl/point_types.h>
-// #include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/PointCloud2.h>
-// #include <pcl/common/io.h>
-// #include "pcl_ros/transforms.h"
-// #include "pcl_ros/impl/transforms.hpp"
 
 const float PI_F = 3.14159265358979f;
 
@@ -32,52 +26,26 @@ public:
     {
       listener.waitForTransform("/world", "/camera_link", ros::Time(0), ros::Duration(3.0));
       listener.lookupTransform("/world", "/camera_link", ros::Time(0), transform_in);
-      // listener2.lookupTransform("/map", "/lens_link", now, transform2_in);
-      // listener2.lookupTransform("/lens_link", "/map", now, transform2_in);
+
     }
     catch (tf::TransformException ex)
     {
-      // ms.pose.position.x = 1.0f;
-      // ms.pose.position.y = 0.0f;
-      // ms.pose.position.z = 0.25f;
       ROS_ERROR("%s", ex.what());
-      // ros::Duration(1.0).sleep();
     }
 
     float diff_secs = static_cast<float>(diff.toSec());
-    // int sign = (diff_secs % 2 == 1) ? 1 : -1;
     float yaw = ROTATION_RATE * diff_secs;
-    // float x = sign * 1.0f;
     if (yaw > 2 * PI_F)
     {
       yaw -= 2 * PI_F;
     }
-    q.setRPY(0.0f, 0.0f, -yaw + PI_F / 2);
+    q.setRPY(0.0f, 0.0f, -yaw + PI_F / 2); // point towards origin
     transform_out.setRotation(q);
     transform_out.setOrigin(tf::Vector3(CAMERA_OFFSET * sin(yaw), CAMERA_OFFSET * cos(yaw), 0.2f));
     br.sendTransform(tf::StampedTransform(transform_out, ros::Time::now(), "world", "base_link"));
 
-    // try
-    // {
-    //   pcl_ros::transformPointCloud("/map", cloud_in, cloud_out, listener2);
-    //   cloud_pub.publish(cloud_out);
-    // }
-    // catch (tf::TransformException ex)
-    // {
-
-    //   ROS_ERROR("%s", ex.what());
-    // }
-    // ms.pose.position.x = 1.0f;
-    // ms.pose.position.y = 0.0f;
-    // ms.pose.position.z = 0.25f;
-    // ms.pose.orientation.x = 0.0f;
-    // ms.pose.orientation.y = 0.0f;
-    // ms.pose.orientation.z = 0.0f;
-    // ms.pose.orientation.w = 1.0f;
-
     ms.model_name = (std::string) "kinect";
     ms.reference_frame = (std::string) "world";
-    // ROS_INFO("xyz: %f %f %f", transform_in.getOrigin().x(), transform_in.getOrigin().y(), transform_in.getOrigin().z());
     ms.pose.position.x = transform_in.getOrigin().x();
     ms.pose.position.y = transform_in.getOrigin().y();
     ms.pose.position.z = transform_in.getOrigin().z();
@@ -87,19 +55,10 @@ public:
     ms.pose.orientation.w = transform_in.getRotation().w();
     camera_pub.publish(ms);
   }
-  // void pcClbk(const sensor_msgs::PointCloud2 &msg)
-  // {
-  //   cloud_in = msg;
-  // }
 
 private:
   ros::NodeHandle n_;
   ros::Publisher camera_pub = n_.advertise<gazebo_msgs::ModelState>("/gazebo/set_model_state", 1);
-  // ros::Publisher cloud_pub = n_.advertise<sensor_msgs::PointCloud2>("/point_cloud_transformed", 1);
-  // ros::Subscriber cloud_sub = n_.subscribe("/camera/depth/points", 1, &CameraMotion::pcClbk, this);
-  // ros::ServiceClient client = n_.serviceClient<gazebo_msgs::ModelState>("/gazebo/set_model_state");
-  // gazebo_msgs::ModelState modelstate;
-  // gazebo_msgs::SetModelState srv;
   ros::Time start = ros::Time::now();
   tf::TransformBroadcaster br;
   tf::TransformListener listener, listener2;
